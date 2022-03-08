03773833f0d7b61628ae2a84a244ffde8adfc5b8
Antoine Lambert <antoine.lambert@etu.imt-nord-europe.fr>
2022-03-08 15:03:47 +0100

    TP2 : uptade (exo3 added)

---
+++ TP2/Exo3/projets.php
@@ -0,0 +1,49 @@
+<?php
+    require_once('template_header.php');
+?>
+        <header>
+            <h1>TP1 : Mon premier site Web</h1>
+            <nav class="menu">
+                <ul>
+                    <li><a href="index.php">ACCUEIL</a></li>
+                    <li><a href="cv.php">CV</a></li>
+                    <li><a id="currentpage" href="projets.php">PROJETS</a></li>
+                    <li><a href="contact.php">CONTACT</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="content">
+            <a class="project" href="https://github.com/AntoineLmbrt">
+                <div class="project_item">
+                    <h3>Project 1</h3>
+                    <img class=project_image src="assets/logo_github.png">
+                </div>
+            </a>
+            <a class="project" href="https://github.com/AntoineLmbrt">
+                <div>
+                    <h3>Project 2</h3>
+                    <img class=project_image src="assets/logo_github.png">
+                </div>
+            </a>
+            <a class="project" href="https://github.com/AntoineLmbrt">
+                <div>
+                    <h3>Project 3</h3>
+                    <img class=project_image src="assets/logo_github.png">
+                </div>
+            </a>
+            <a class="project" href="https://github.com/AntoineLmbrt">
+                <div>
+                    <h3>Project 4</h3>
+                    <img class=project_image src="assets/logo_github.png">
+                </div>
+            </a>
+            <a class="project" href="https://github.com/AntoineLmbrt">
+                <div>
+                    <h3>Project 5</h3>
+                    <img class=project_image src="assets/logo_github.png">
+                </div>
+            </a>
+        </div>
+<?php
+    require_once('template_footer.php');
+?>
---
+++ TP2/Exo3/template_header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <meta charset="utf-8">
+        <title>TP1</title>
+
+        <!-- CSS files -->
+        <link rel="stylesheet" href="style.css">
+        
+    </head>
+    <body>
