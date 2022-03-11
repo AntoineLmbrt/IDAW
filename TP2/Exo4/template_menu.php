0a65064605d823afd6c767ead07e5ae0eb6f5505
Antoine Lambert <antoine.lambert@etu.imt-nord-europe.fr>
2022-03-11 09:29:09 +0100

    TP2 : update (exo4 css fixed)

--- TP2/Exo4/template_menu.php
+++ TP2/Exo4/template_menu.php
@@ -3,7 +3,6 @@
         <li><a href="index.php">ACCUEIL</a></li>
         <li><a href="cv.php">CV</a></li>
         <li><a href="projets.php">PROJETS</a></li>
-        <li><a href="contact.php">CONTACT</a></li>
         <li><a href="infos-technique.php">INFOS</a></li>
     </ul>
 </nav>
